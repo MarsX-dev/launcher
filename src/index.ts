5fb18d0ac70c655aa6f6eed555ff9d6d488f3094
Marat <marat@marsx.dev>
2022-03-19 11:37:13 -0700

    Export types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { config } from './configuration';
+export { config, ImportProjectConfig, Configuration } from './configuration';
 export { compileSfcSource, transpileTypescript } from './utils/compile';
-export { parseSFC, serializeSfc } from './utils/sfc';
+export { parseSFC, serializeSfc, SfcBlock, SfcIdentity, SfcSource, parseSfcIdentity, serializeSfcIdentity } from './utils/sfc';
 export * as utils from './utils/utils';
-export { convertSfcToV3, convertV3ToSfc } from './utils/v3';
+export { convertSfcToV3, convertV3ToSfc, V3MongoBlock } from './utils/v3';
